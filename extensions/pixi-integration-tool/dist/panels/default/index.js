061984285f40d5becea0fb85b6327df75891459e
kakalak33 <tu.tran@enotion.io>
2023-12-27 21:51:57 +0700

    - update logic build pixi tree from cocos scene

--- extensions/pixi-integration-tool/dist/panels/default/index.js
+++ extensions/pixi-integration-tool/dist/panels/default/index.js
@@ -1,14 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
 const fs_extra_1 = require("fs-extra");
 const path_1 = require("path");
 const vue_1 = require("vue");
+const lodash_1 = require("lodash");
 const panelDataMap = new WeakMap();
 /**
  * @zh 如果希望兼容 3.3 之前的版本可以使用下方的代码
  * @en You can add the code below if you want compatibility with versions prior to 3.3
  */
 // Editor.Panel.define = Editor.Panel.define || function(options: any) { return options }
+const OUTPUT_DIRECTORY = (0, path_1.join)(__dirname, '../../../data');
 module.exports = Editor.Panel.define({
     listeners: {
         show() { console.log('show'); },
@@ -30,12 +33,14 @@ module.exports = Editor.Panel.define({
                 },
                 methods: {
                     onConfirm() {
+                        (0, child_process_1.exec)('npm run test');
                         console.log('==== Start reading scene ====');
                         if (!this.sceneDir)
                             return console.log('==== Scene directory is empty ====');
-                        const contentArr = getSceneArray(this.sceneDir);
-                        const nodesArr = getNodesFromScene(contentArr);
-                        console.log(nodesArr);
+                        const sceneObjArr = getSceneArray(this.sceneDir);
+                        console.log('==== Build Pixi Tree From Scene ====');
+                        const pixiTree = buildPixiTreeFromSceneArray(sceneObjArr);
+                        console.log('==== Build Pixi Tree From Scene Successfully ====');
                     },
                     handleFileDirectory(e) {
                         if (e.target.value) {
@@ -56,8 +61,32 @@ function getSceneArray(filePath) {
     return fileContentArray;
 }
 function getNodesFromScene(sceneArray) {
-    const nodesArray = sceneArray.filter((sceneObj) => {
+    const nodesArray = sceneArray.filter((sceneObj, index) => {
         return sceneObj.__type__ === 'cc.Node';
     });
-    return nodesArray || [];
+    return nodesArray;
+}
+function buildPixiTreeFromSceneArray(sceneArray) {
+    // mutate data on _sceneArr to link components, link nodes
+    const _sceneArr = sceneArray.slice();
+    for (let index = 0; index < _sceneArr.length; index++) {
+        const nodeObj = _sceneArr[index];
+        if (!nodeObj || nodeObj.__type__ !== 'cc.Node')
+            continue;
+        // link components to node
+        nodeObj._components.forEach(({ __id__ }, index) => {
+            nodeObj._components[index] = _sceneArr[__id__];
+            nodeObj._components[index].node = nodeObj;
+        });
+        // link node to node
+        nodeObj._children.forEach(({ __id__ }, index) => {
+            nodeObj._children[index] = _sceneArr[__id__];
+            _sceneArr[__id__]._parent = nodeObj;
+        });
+    }
+    (0, lodash_1.remove)(_sceneArr, function (sceneObj) {
+        if (sceneObj.__type__ !== 'cc.Node')
+            return true;
+    });
+    return _sceneArr;
 }
--- extensions/pixi-integration-tool/src/panels/default/index.ts
+++ extensions/pixi-integration-tool/src/panels/default/index.ts
@@ -1,12 +1,15 @@
-import { PathOrFileDescriptor, outputJSONSync, readFileSync, readJsonSync } from 'fs-extra';
+import { exec } from 'child_process';
+import { PathOrFileDescriptor, readFileSync } from 'fs-extra';
 import { join } from 'path';
 import { createApp, App } from 'vue';
+import { remove } from 'lodash';
 const panelDataMap = new WeakMap<any, App>();
 /**
  * @zh 如果希望兼容 3.3 之前的版本可以使用下方的代码
  * @en You can add the code below if you want compatibility with versions prior to 3.3
  */
 // Editor.Panel.define = Editor.Panel.define || function(options: any) { return options }
+const OUTPUT_DIRECTORY = join(__dirname, '../../../data');
 module.exports = Editor.Panel.define({
     listeners: {
         show() { console.log('show'); },
@@ -28,14 +31,14 @@ module.exports = Editor.Panel.define({
                 },
                 methods: {
                     onConfirm() {
+                        exec('npm run test');
                         console.log('==== Start reading scene ====');
                         if (!this.sceneDir) return console.log('==== Scene directory is empty ====');
+                        const sceneObjArr: Array<SceneObject> = getSceneArray(this.sceneDir);
+                        console.log('==== Build Pixi Tree From Scene ====');
+                        const pixiTree = buildPixiTreeFromSceneArray(sceneObjArr);
+                        console.log('==== Build Pixi Tree From Scene Successfully ====');
 
-                        const contentArr: Array<SceneObject> = getSceneArray(this.sceneDir);
-
-                        const nodesArr = getNodesFromScene(contentArr);
-
-                        console.log(nodesArr);
                     },
                     handleFileDirectory(e: any) {
                         if (e.target.value) {
@@ -55,14 +58,15 @@ interface SceneObject {
     __type__: String,
     _active: Boolean,
     _children: Array<any>,
-    _components: Array<{ id: number }>,
+    _components: Array<{ __id__: number } | any>,
     _name: String,
-    _parent: Array<{ id: number }>,
+    _parent: { __id__: number } | any,
     /*
     _lpos: {__type__: "cc.Vec3", x: 5, y: -78, z: 0}
     _lrot: {__type__: "cc.Quat", x: 0, y: 0, z: 0, w: 1}
     _lscale: {__type__: "cc.Vec3", x: 1, y: 1, z: 1}
     */
+    treeIndex: number;
 }
 
 function getSceneArray(filePath: PathOrFileDescriptor): Array<SceneObject> {
@@ -71,9 +75,34 @@ function getSceneArray(filePath: PathOrFileDescriptor): Array<SceneObject> {
     return fileContentArray;
 }
 
-function getNodesFromScene(sceneArray: Array<SceneObject>) {
-    const nodesArray = sceneArray.filter((sceneObj: SceneObject) => {
+function getNodesFromScene(sceneArray: Array<SceneObject>): Array<SceneObject> {
+    const nodesArray = sceneArray.filter((sceneObj: SceneObject, index) => {
         return sceneObj.__type__ === 'cc.Node';
     });
-    return nodesArray || [];
+    return nodesArray;
+}
+
+function buildPixiTreeFromSceneArray(sceneArray: Array<SceneObject>) {
+    // mutate data on _sceneArr to link components, link nodes
+    const _sceneArr = sceneArray.slice();
+    for (let index = 0; index < _sceneArr.length; index++) {
+        const nodeObj = _sceneArr[index];
+        if (!nodeObj || nodeObj.__type__ !== 'cc.Node') continue;
+        // link components to node
+        nodeObj._components.forEach(({ __id__ }, index) => {
+            nodeObj._components[index] = _sceneArr[__id__];
+            nodeObj._components[index].node = nodeObj;
+        });
+        // link node to node
+        nodeObj._children.forEach(({ __id__ }, index) => {
+            nodeObj._children[index] = _sceneArr[__id__];
+            _sceneArr[__id__]._parent = nodeObj
+        });
+    }
+
+    remove(_sceneArr, function (sceneObj) {
+        if (sceneObj.__type__ !== 'cc.Node') return true;
+    });
+
+    return _sceneArr;
 }
